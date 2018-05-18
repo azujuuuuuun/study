4f55652c04f50fd16ff6fbdb528578f96814659a
azujuuuuuun <azujuuuuuun@gmail.com>
2018-05-18 23:47:14 +0900

    feat(App):todoのフィルタ機能を追加

--- react/todo/src/App.js
+++ react/todo/src/App.js
@@ -8,6 +8,8 @@ export default class App extends React.Component {
       text: '',
       todos: [],
       id: 1,
+      visibleToDos: [],
+      filter: 'all',
     };
   }
 
@@ -24,28 +26,59 @@ export default class App extends React.Component {
       text: this.state.text,
       isCompleted: false,
     };
-    this.setState({
+    const newState = {
       text: '',
       todos: this.state.todos.concat(todo),
       id: this.state.id + 1,
-    });
+    };
+    if (this.state.filter === 'all' || this.state.filter === 'incompleted') {
+      newState.visibleToDos = this.state.visibleToDos.concat(todo);
+    }
+    this.setState(newState);
   }
 
   toggleToDo(id) {
+    const newToDos = this.state.todos.map((todo) => {
+      const t = todo;
+      if (t.id === id) {
+        t.isCompleted = !t.isCompleted;
+      }
+      return t;
+    });
+    let newVisibleToDos = [];
+    if (this.state.filter === 'all') {
+      newVisibleToDos = newToDos;
+    } else if (this.state.filter === 'completed') {
+      newVisibleToDos = newToDos.filter(todo => todo.isCompleted);
+    } else {
+      newVisibleToDos = newToDos.filter(todo => !todo.isCompleted);
+    }
     this.setState({
-      todos: this.state.todos.map((todo) => {
-        const t = todo;
-        if (t.id === id) {
-          t.isCompleted = !t.isCompleted;
-        }
-        return t;
-      }),
+      todos: newToDos,
+      visibleToDos: newVisibleToDos,
     });
   }
 
   removeToDo(id) {
     this.setState({
       todos: this.state.todos.filter(todo => todo.id !== id),
+      visibleToDos: this.state.visibleToDos.filter(todo => todo.id !== id),
+    });
+  }
+
+  changeFilter(filter) {
+    const newToDos = this.state.todos;
+    let newVisibleToDos = [];
+    if (filter === 'all') {
+      newVisibleToDos = newToDos;
+    } else if (filter === 'completed') {
+      newVisibleToDos = newToDos.filter(todo => todo.isCompleted);
+    } else {
+      newVisibleToDos = newToDos.filter(todo => !todo.isCompleted);
+    }
+    this.setState({
+      visibleToDos: newVisibleToDos,
+      filter: filter,
     });
   }
 
@@ -56,7 +89,12 @@ export default class App extends React.Component {
           <input type="text" value={this.state.text} onChange={(e) => this.onChange(e)} />
           <button onClick={(e) => this.addToDo(e)}>+</button>
         </form>
-        <ToDoList todos={this.state.todos} toggleToDo={(id) => this.toggleToDo(id)} removeToDo={(id) => this.removeToDo(id)} />
+        <ToDoList todos={this.state.visibleToDos} toggleToDo={(id) => this.toggleToDo(id)} removeToDo={(id) => this.removeToDo(id)} />
+        <div>
+          <button onClick={() => this.changeFilter('all')}>all</button>
+          <button onClick={() => this.changeFilter('completed')}>completed</button>
+          <button onClick={() => this.changeFilter('incompleted')}>incompleted</button>
+        </div>
       </div>
     );
   }
